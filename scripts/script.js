fe2f21411aceeaeb5f6b362e1a6932389c5a1157
Alinda <AlindaHeng@gmail.com>
2021-03-29 00:38:14 -0700

    Script that handles responses

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,13 @@
 function test() {
-	// Something here!
+	// https://javascript.info/fetch
+	url = "http://127.0.0.1:5000/"
+	let promise = await fetch(url)
+
+	if (response.ok) {
+		let text = await response.text();
+	}
+	else {
+		alert("HTTP-Error: " + response.status);
+	}
+	console.log("retrieved " + text);
 }
