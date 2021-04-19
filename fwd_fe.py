05470ed42ae17847bcfc9a604c38d27819028a4a
Alinda <AlindaHeng@gmail.com>
2021-04-18 23:38:01 -0700

    Request and display response from backend

--- fwd_fe.py
+++ fwd_fe.py
@@ -4,10 +4,12 @@ This app returns the length of a given string.
 """
 from flask import Flask, request, render_template
 from flask_restful import Resource, Api
+import requests
 
 app = Flask(__name__)
 api = Api(app)
 
+backend_ip = "http://localhost:5000/"
 some_user = { 'name': 'Guest' }
 def_val = "Hello World!"
 
@@ -17,8 +19,9 @@ def index():
 
 @app.route('/', methods = ['POST'])
 def send_form():
-	data = request.form['num_input']
-	return render_template("index.html", user=some_user, form_in=data, res=len(data))
+	user_str = request.form['usr_input']
+	res = requests.post(backend_ip, data=request.form)
+	return render_template("index.html", user=some_user, form_in=user_str, res=res.text)
 
 if __name__ == '__main__':
     app.run(debug=True, host="localhost", port=8000)
