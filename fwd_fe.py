d8eb25ebf2fa72934b94740c74713c8ee626b6d9
Alinda <AlindaHeng@gmail.com>
2021-04-18 22:43:45 -0700

    Templates

--- fwd_fe.py
+++ fwd_fe.py
@@ -8,9 +8,17 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+some_user = { 'name': 'Guest' }
+def_val = "Hello World!"
+
 @app.route('/')
 def index():
-	return render_template("index.html")
+	return render_template("index.html", user=some_user, form_in=def_val)
+
+@app.route('/', methods = ['POST'])
+def send_form():
+	data = request.form['num_input']
+	return render_template("index.html", user=some_user, form_in=data, res=len(data))
 
 if __name__ == '__main__':
     app.run(debug=True, host="localhost", port=8000)
